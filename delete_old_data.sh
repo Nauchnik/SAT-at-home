0b8751dd72e122ab46c6fa4a5c24308c172794d3
Nauchnik <zaikin.icc@gmail.com>
2013-10-04 14:21:32 +0900

    Update for inc72 experiment
    work with diag10 and inc0 was removed

--- delete_old_data.sh
+++ delete_old_data.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 db="boinc_pdsat"
-sql1="UPDATE RESEARCH_PROGRESS
-  SET WORKUNITS_DELETED = WORKUNITS_DELETED + (SELECT COUNT(*) FROM workunit WHERE MOD_TIME < DATE(NOW() - INTERVAL 30 DAY) AND ASSIMILATE_STATE > 0 AND NAME LIKE '%diag10_2%')
-  WHERE WORKUNITS_MASK = '%diag10_2%'"
-sql2="DELETE FROM result WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND validate_state > 0"
-sql3="DELETE FROM workunit WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND assimilate_state > 0"
+
+ls_10_3_inc70_update="UPDATE RESEARCH_PROGRESS
+   SET WORKUNITS_DELETED = WORKUNITS_DELETED + (SELECT COUNT(*) FROM workunit WHERE MOD_TIME < DATE(NOW() - INTERVAL 30 DAY) AND ASSIMILATE_STATE > 0 AND NAME LIKE '%ls_10_3_inc70%')
+ WHERE WORKUNITS_MASK = '%ls_10_3_inc70%'"
+ls_10_3_inc70_result_delete="DELETE FROM result WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND validate_state > 0 AND NAME LIKE '%ls_10_3_inc70%'"
+ls_10_3_inc70_workunit_delete="DELETE FROM workunit WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND assimilate_state > 0 AND NAME LIKE '%ls_10_3_inc70%'"
+
+ls_10_3_inc72_update="UPDATE RESEARCH_PROGRESS
+   SET WORKUNITS_DELETED = WORKUNITS_DELETED + (SELECT COUNT(*) FROM workunit WHERE MOD_TIME < DATE(NOW() - INTERVAL 30 DAY) AND ASSIMILATE_STATE > 0 AND NAME LIKE '%ls_10_3_inc72%')
+ WHERE WORKUNITS_MASK = '%ls_10_3_inc72%'"
+ls_10_3_inc72_result_delete="DELETE FROM result WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND validate_state > 0 AND NAME LIKE '%ls_10_3_inc72%'"
+ls_10_3_inc72_workunit_delete="DELETE FROM workunit WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND assimilate_state > 0 AND NAME LIKE '%ls_10_3_inc72%'"
+
+orphan_results_delete="DELETE FROM result WHERE workunitid NOT IN(SELECT id FROM workunit)"
+
 mysql --defaults-extra-file=/var/lib/boinc/pdsat/.my.cnf << EOF
     use $db;
-    $sql1;
-    $sql2;
-    $sql3;
+    $ls_10_3_inc70_update;
+    $ls_10_3_inc70_result_delete;
+    $ls_10_3_inc70_workunit_delete;
+    $ls_10_3_inc72_update;
+    $ls_10_3_inc72_result_delete;
+    $ls_10_3_inc72_workunit_delete;
+    $orphan_results_delete;
 EOF
