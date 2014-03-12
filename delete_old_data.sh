228997558d638cc26b9cfd8d0cd91a74fa58d99c
Nauchnik <zaikin.icc@gmail.com>
2014-03-12 13:18:23 +0900

    interval for bivium was decreased from 30 to 20 days

--- delete_old_data.sh
+++ delete_old_data.sh
@@ -8,10 +8,10 @@ ls_10_3_inc72_result_delete="DELETE FROM result WHERE mod_time < DATE(NOW() - IN
 ls_10_3_inc72_workunit_delete="DELETE FROM workunit WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND assimilate_state > 0 AND NAME LIKE '%ls_10_3_inc72%'"
 
 bivium_update="UPDATE RESEARCH_PROGRESS
-   SET WORKUNITS_DELETED = WORKUNITS_DELETED + (SELECT COUNT(*) FROM workunit WHERE MOD_TIME < DATE(NOW() - INTERVAL 30 DAY) AND ASSIMILATE_STATE > 0 AND NAME LIKE '%bivium%')
+   SET WORKUNITS_DELETED = WORKUNITS_DELETED + (SELECT COUNT(*) FROM workunit WHERE MOD_TIME < DATE(NOW() - INTERVAL 20 DAY) AND ASSIMILATE_STATE > 0 AND NAME LIKE '%bivium%')
  WHERE WORKUNITS_MASK = '%bivium%'"
-bivium_result_delete="DELETE FROM result WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND validate_state > 0 AND NAME LIKE '%bivium%'"
-bivium_workunit_delete="DELETE FROM workunit WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND assimilate_state > 0 AND NAME LIKE '%bivium%'"
+bivium_result_delete="DELETE FROM result WHERE mod_time < DATE(NOW() - INTERVAL 20 DAY) AND validate_state > 0 AND NAME LIKE '%bivium%'"
+bivium_workunit_delete="DELETE FROM workunit WHERE mod_time < DATE(NOW() - INTERVAL 20 DAY) AND assimilate_state > 0 AND NAME LIKE '%bivium%'"
 
 orphan_results_delete="DELETE FROM result WHERE workunitid NOT IN(SELECT id FROM workunit)"
 
