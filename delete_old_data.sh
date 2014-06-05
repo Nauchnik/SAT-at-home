d5a2d6d8c2982e961a5303783e407c16bb357030
Nauchnik <zaikin.icc@gmail.com>
2014-06-05 18:18:14 +0900

    + a5_1_114

--- delete_old_data.sh
+++ delete_old_data.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 db="boinc_pdsat"
 
-ls_10_3_inc72_update="UPDATE RESEARCH_PROGRESS
-   SET WORKUNITS_DELETED = WORKUNITS_DELETED + (SELECT COUNT(*) FROM workunit WHERE MOD_TIME < DATE(NOW() - INTERVAL 30 DAY) AND ASSIMILATE_STATE > 0 AND NAME LIKE '%ls_10_3_inc72%')
- WHERE WORKUNITS_MASK = '%ls_10_3_inc72%'"
-ls_10_3_inc72_result_delete="DELETE FROM result WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND validate_state > 0 AND NAME LIKE '%ls_10_3_inc72%'"
-ls_10_3_inc72_workunit_delete="DELETE FROM workunit WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND assimilate_state > 0 AND NAME LIKE '%ls_10_3_inc72%'"
-
 bivium_update="UPDATE RESEARCH_PROGRESS
    SET WORKUNITS_DELETED = WORKUNITS_DELETED + (SELECT COUNT(*) FROM workunit WHERE MOD_TIME < DATE(NOW() - INTERVAL 20 DAY) AND ASSIMILATE_STATE > 0 AND NAME LIKE '%bivium%')
  WHERE WORKUNITS_MASK = '%bivium%'"
 bivium_result_delete="DELETE FROM result WHERE mod_time < DATE(NOW() - INTERVAL 20 DAY) AND validate_state > 0 AND NAME LIKE '%bivium%'"
 bivium_workunit_delete="DELETE FROM workunit WHERE mod_time < DATE(NOW() - INTERVAL 20 DAY) AND assimilate_state > 0 AND NAME LIKE '%bivium%'"
 
+a5_1_114_update="UPDATE RESEARCH_PROGRESS
+   SET WORKUNITS_DELETED = WORKUNITS_DELETED + (SELECT COUNT(*) FROM workunit WHERE MOD_TIME < DATE(NOW() - INTERVAL 30 DAY) AND ASSIMILATE_STATE > 0 AND NAME LIKE '%a5_1_114%')
+ WHERE WORKUNITS_MASK = '%a5_1_114%'"
+a5_1_114_delete="DELETE FROM result WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND validate_state > 0 AND NAME LIKE '%a5_1_114%' "
+a5_1_114_workunit_delete="DELETE FROM workunit WHERE mod_time < DATE(NOW() - INTERVAL 30 DAY) AND assimilate_state > 0 AND NAME LIKE '%a5_1_114%'"
+
 orphan_results_delete="DELETE FROM result WHERE workunitid NOT IN(SELECT id FROM workunit)"
 
 mysql --defaults-extra-file=/var/lib/boinc/pdsat/.my.cnf << EOF
     use $db;
-    $ls_10_3_inc72_update;
-    $ls_10_3_inc72_result_delete;
-    $ls_10_3_inc72_workunit_delete;
     $bivium_update;
     $bivium_result_delete;
     $bivium_workunit_delete;
+    $a5_1_114_update;
+    $a5_1_114_result_delete;
+    $a5_1_114_workunit_delete;
     $orphan_results_delete;
 EOF
