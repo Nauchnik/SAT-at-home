0e14a60da9e83516ddaddd44a69e1697424afee1
Oleg Zaikin <zaikin.icc@gmail.com>
2014-10-04 23:39:23 +0800

    + publications.php

---
+++ publications.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once("../inc/util.inc");
+require_once("../inc/translation.inc");
+
+page_head(tra('Publications and materials'));
+
+echo "
+<p class='need_some_space'>
+<ul>
+<br>
+<b>2014</>
+<li><a href = 'files/Semenov_Zaikin_VSPU_2014.pdf'>Семенов А.А., Заикин О.С., Отпущенников И.В., Кочемазов С.Е. Применение алгоритмов решения проблемы булевой выполнимости (SAT) к комбинаторным задачам // Труды XII Всероссийского совещания по проблемам управления. Изд-во ИПУ РАН, 2014. С. 7361-7374</a>
+<li><a href = 'http://num-meth.srcc.msu.ru/zhurnal/tom_2014/pdf/v15r103.pdf'> Заикин О.С., Семенов А.А. Применение метода Монте-Карло к прогнозированию времени параллельного решения проблемы булевой выполнимости // Вычислительные методы и программирование: новые вычислительные технологии. 2014. Т. 15. Вып. 1. С. 22-35. </a>
+</br>
+<br>
+<b>2013</b>
+<li><a href = 'http://arxiv.org/abs/1308.0761'>Alexander Semenov and Oleg Zaikin. On estimating total time to solve SAT in distributed computing environments: Application to the SAT@home project. arXiv:1308.0761 [cs.AI]</a>
+<li><a href = 'http://ubs.mtas.ru/archive/search_results_new.php?publication_id=19025'>Заикин О.С., Семенов А.А., Посыпкин М.А. Процедуры построения декомпозиционных множеств для распределенного решения SAT-задач в проекте добровольных вычислений SAT@home // Управление большими системами. Выпуск 43. М.: ИПУ РАН, 2013. С. 138-156.</a>
+<br>
+<br>
+<b>2012</b>
+<li><a href = 'http://trv-science.ru/2012/12/11/okhotniki-za-kvadratami/#respond'>А. Андреев, О. Заикин. Охотники за квадратами // Троицкий вариант — Наука. 2012, № 118, 'Информатика'. С. 6-7.</a>
+<li><a href = 'http://www.unn.ru/e-library/vestnik.html'>Заикин О.С., Посыпкин М.А., Семёнов А.А., Храпов Н.П. Опыт организации добровольных вычислений на примере проектов OPTIMA@home и SAT@home // Вестник ННГУ. № 5(2). 2012. С. 338-346</a>
+<li><a href = 'http://isu.ru/journal/downloadArticle?article=_cfdda66c87824d579e53a33068018e1d&lang=rus'>Семенов А.А., Заикин О.С. Алгоритмы построения декомпозиционных множеств для крупноблочного распараллеливания SAT-задач // Известия ИГУ, Серия «Математика» № 4, 2012. С. 79-94.</a>
+<li><a href = 'files/Grid2012_SAT_at_home_promotion.ppt'>BOINC projects promotion – SAT@home experience. Report from conference Gtid'2012 in Dubna.</a>
+<li><a href = 'files/SAT@home_Optima@home_CAD-CAM-CAE_2012.pdf'>О.С. Заикин, М.А. Посыпкин, А.А. Семёнов, Н.П. Храпов. Организация добровольных вычислений на платформе BOINC на примере проектов OPTIMA@home и SAT@home // CAD/CAM/CAE Observer # 3 (71) / 2012. С. 87-92.</a>
+<li><a href = 'http://journals.agh.edu.pl/csci/article/view/37'>Posypkin, Mikhail and Semenov, Alexander and Zaikin, Oleg (2012) Using BOINC desktop grid to solve large scale SAT problems. Computer Science, 13 (1). pp. 25-34.</a>
+<br>
+<br>
+<b>2011</b>
+<li><a href = 'files/Rainbow_tables_Semenov_14-12-2011.pdf'>А.А. Семенов. Криптоанализ с применением rainbow-таблиц. Доклад в Институте математики им. С.Л. Соболева СО РАН 14.12.2011.</a>
+<li><a href = 'http://www.gridclub.ru/library/publication.2011-11-13.0947357099/view'>О.С. Заикин. Решение задач обращения некоторых дискретных функций в проекте добровольных вычислений SAT@home // Современные информационные технологии и ИТ-образование. Сборник избранных докладов научно-практической конференции: учебно-методическое пособие. - М.: ИНТУИТ.РУ, 2011. C. 960-965</a>
+<li><a href = 'files/Posypkin_Semenov_Zaikin_CGW_2011.pdf'>Mikhail Posypkin, Alexander Semenov, Oleg Zaikin. Using BOINC Desktop Grid to Solve Large Scale SAT Problems. 
+Poster at Cracow Grid Workshop 2011.</a>
+<li><a href = 'http://link.springer.com/chapter/10.1007%2F978-3-642-23178-0_43'> Alexander Semenov, Oleg Zaikin, Dmitry Bespalov, Mikhail Posypkin. Parallel Logical Cryptanalysis of the Generator A5/1 in BNB-Grid System // Lecture Notes in Computer Science. Vol. 6873, 2011, pp 473-483</a>
+</ul>
+</p>";
+                                                                                                                                                                                            
+page_tail();
+?>
